c795d15a063b8f30ee2e8063aa3998b5dd40dcb3
lucas <lucas.vergara@usm.cl>
2021-08-23 19:36:33 -0400

    ready

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const axios = require('axios');
+const { clearInterval } = require('timers');
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const originNumber = process.env.TWILIO_NUMBER;
+const destinationNumber = process.env.MY_NUMBER;
+const apiUrl = process.env.API_URL;
+const client = require('twilio')(accountSid, authToken);
+
+const mainLoop = setInterval( async () => {
+    const {data} = await axios.get(apiUrl);
+    console.log(data.status_history.length)
+    if (data.status_history.length > 7) {
+        const resp = await client.messages
+            .create({
+                body: `El pedido cambio de estado a: ${data.status}!!!`,
+                from: originNumber,
+                to: destinationNumber
+            })
+        clearInterval(mainLoop);
+    }
+}, 60000);
+
